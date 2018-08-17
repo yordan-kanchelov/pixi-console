5c147d4258a2b2eb07c2575d01de6603f8e2db88
Jordan Kanchelov <jkanchelov@gmail.com>
2018-08-17 19:11:27 +0300

    updated build

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,7 +1,3 @@
 import { PixiConsole } from "./app/pixi-console";
 import { PixiConsoleConfig } from "./app/pixi-console-config";
-declare const _default: {
-    PixiConsole: typeof PixiConsole;
-    PixiConsoleConfig: typeof PixiConsoleConfig;
-};
-export default _default;
+export { PixiConsole, PixiConsoleConfig };
--- dist/index.js
+++ dist/index.js
@@ -1,8 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var pixi_console_1 = require("./app/pixi-console");
+exports.PixiConsole = pixi_console_1.PixiConsole;
 var pixi_console_config_1 = require("./app/pixi-console-config");
-exports.default = {
-    PixiConsole: pixi_console_1.PixiConsole,
-    PixiConsoleConfig: pixi_console_config_1.PixiConsoleConfig
-};
+exports.PixiConsoleConfig = pixi_console_config_1.PixiConsoleConfig;
