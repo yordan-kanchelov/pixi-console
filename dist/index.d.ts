b99bb7018572968fb88ead87d10f02e299c0cb18
Jordan Kanchelov <jkanchelov@gmail.com>
2018-08-30 07:33:06 +0300

    removed dist folder

--- dist/index.d.ts
+++
@@ -1,3 +0,0 @@
-import { PixiConsole } from "./app/pixi-console";
-import { PixiConsoleConfig } from "./app/pixi-console-config";
-export { PixiConsole, PixiConsoleConfig };
--- dist/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var pixi_console_1 = require("./app/pixi-console");
-exports.PixiConsole = pixi_console_1.PixiConsole;
-var pixi_console_config_1 = require("./app/pixi-console-config");
-exports.PixiConsoleConfig = pixi_console_config_1.PixiConsoleConfig;
