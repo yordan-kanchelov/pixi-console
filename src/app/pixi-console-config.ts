d0eddb7fa18f7c7d4ca8e147bfe147d2e4db49fc
Jordan Kanchelov <jkanchelov@gmail.com>
2018-09-03 03:19:02 +0300

    added fontErrorColor as customization option

--- src/app/pixi-console-config.ts
+++ src/app/pixi-console-config.ts
@@ -2,6 +2,8 @@ export class PixiConsoleConfig {
     consoleWidth: number = 1280;
     consoleHeight: number = 720;
     fontSize: number = 30;
+    fontColor: number = 0xffffff;
+    fontErrorColor: number = 0xff0000;
 
     // events options
     showOnError: boolean = true;
@@ -11,7 +13,6 @@ export class PixiConsoleConfig {
     // customization
     consoleAlpha: number = 0.2;
     backgroundColor: number = 0xffffff;
-    defaultPrintColor: number = 0xffffff;
 
     scrollingYStep = 40;
     textStartingX = 10;
