ab4207fb90a64e4bafe9c4985f84015959a20c8c
yordan.kanchelov <yordan.kanchelov@gmail.com>
2023-12-28 11:04:47 +0200

    wip

--- webpack.config.js
+++ webpack.config.js
@@ -1,61 +1,61 @@
-const path = require('path');
+const path = require("path");
 
-const mode = process.env.NODE_ENV || 'production';
+const mode = process.env.NODE_ENV || "production";
 
 // Base configuration
 const baseConfig = {
-  mode: 'production',
-  entry: './src/index.ts',
-  devtool: mode === 'development' ? 'inline-source-map' : 'source-map',
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        use: 'ts-loader',
-        exclude: /node_modules/,
-      },
-    ],
-  },
-  resolve: {
-    extensions: ['.ts', '.tsx', '.js'],
-  },
-  externals: ['pixi.js']
-  
+    mode: "production",
+    entry: "./src/index.ts",
+    devtool: mode === "development" ? "inline-source-map" : "source-map",
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: "ts-loader",
+                exclude: /node_modules/,
+            },
+        ],
+    },
+    resolve: {
+        extensions: [".ts", ".tsx", ".js"],
+    },
+    externals: ["pixi.js"],
 };
 
 // Configuration for UMD
 const umdConfig = {
-  ...baseConfig,
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'pixi-console.umd.js',
-    library: 'PixiConsole',
-    libraryTarget: 'umd',
-    globalObject: 'this',
-  },
+    ...baseConfig,
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "pixi-console.umd.js",
+        library: "PixiConsole",
+        libraryTarget: "umd",
+        globalObject: "this",
+    },
 };
 
-// Configuration for ES Modules
-const esConfig = {
-  ...baseConfig,
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'pixi-console.esm.js',
-    libraryTarget: 'module',
-  },
-  experiments: {
-    outputModule: true,
-  },
-};
+// disabled for now , due to some errors
+// // Configuration for ES Modules
+// const esConfig = {
+//     ...baseConfig,
+//     output: {
+//         path: path.resolve(__dirname, "dist"),
+//         filename: "pixi-console.mjs",
+//         libraryTarget: "module",
+//     },
+//     experiments: {
+//         outputModule: true,
+//     },
+// };
 
 // Configuration for CommonJS
 const commonJsConfig = {
-  ...baseConfig,
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'pixi-console.cjs.js',
-    libraryTarget: 'commonjs2',
-  },
+    ...baseConfig,
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "pixi-console.js",
+        libraryTarget: "commonjs2",
+    },
 };
 
 module.exports = [umdConfig, esConfig, commonJsConfig];
